807c24fe07d067a29f7445c2259e6e39694a6b6b
guohw <guohw@codoon.com>
2016-08-23 09:05:15 +0800

    异常保护

--- www/logic/views.py
+++ www/logic/views.py
@@ -11,7 +11,7 @@ from common.oss_tools import OSSTools
 from wx_base.backends.common import CommonHelper
 from wx_base.backends.dj import Helper
 from wx_base import WxPayConf_pub
-from user.user_tools import sns_userinfo_with_userinfo, get_userid_by_openid, is_vip
+from user.user_tools import sns_userinfo_with_userinfo, get_userid_by_openid, is_vip, get_user_profile_by_user_id
 from django.forms.models import model_to_dict
 from user.models import Bind, Profile, ProfileExt
 
@@ -23,25 +23,28 @@ def index(request):
 		logging.error('Cant find user_id by openid: %s when post_job' % request.openid)
 		return HttpResponse("十分抱歉，获取用户信息失败，请重试。重试失败请联系客服人员")
 
+	profile = get_user_profile_by_user_id(user_id=user_id, need_default=True)
+
 	vip_job_from_point = convert.str_to_int(request.GET.get('from', '0'), 0)  # 有from时，则为翻页，无时，则为首页
 	number_limit = convert.str_to_int(request.GET.get('limit', '10'), 10)  # 异常情况下，或者不传的情况下，默认为10
 	own_job = {}
 	# 取本人发布过的，并且有效的简历
-	if vip_job_from_point != 0:  # 不是首页的话，翻页不需要继续找own_jobs了
+	if vip_job_from_point != 0:  # 不是首页的话，翻页不需要继续找own_job了
 		own_job = []
 	else:  # 首页
-		own_jobs = Job.objects.filter(user_id=user_id).order_by('-id')[0]
-		own_job['city'] = own_jobs.city + " " + own_jobs.district
-		own_job['company_name'] = own_jobs.company_name
-		own_job['job_title'] = own_jobs.job_title
-		own_job['education'] = own_jobs.education
-		own_job['work_experience'] = own_jobs.work_experience
-		own_job['salary'] = own_jobs.salary
-		own_job['create_time'] = convert.format_time(own_jobs.create_time)
-		own_job['job_uuid'] = own_jobs.uuid
-		profile = Profile.objects.filter(id=user_id)[0]
-		own_job['username'] = profile.real_name
-		own_job['portrait'] = profile.portrait
+		own_jobs = Job.objects.filter(user_id=user_id).order_by('-id')[:1]
+		if own_jobs:
+			own_job = own_jobs[0]
+			own_job['city'] = own_job.city + " " + own_job.district
+			own_job['company_name'] = own_job.company_name
+			own_job['job_title'] = own_job.job_title
+			own_job['education'] = own_job.education
+			own_job['work_experience'] = own_job.work_experience
+			own_job['salary'] = own_job.salary
+			own_job['create_time'] = convert.format_time(own_job.create_time)
+			own_job['job_uuid'] = own_job.uuid
+			own_job['username'] = profile.real_name
+			own_job['portrait'] = profile.portrait
 
 	# 按发布时间去取VIP发布简历 －－ 以后从缓存中取
 	vip_jobs = VipJobList.objects.all().order_by('-pub_time')[vip_job_from_point:number_limit + vip_job_from_point]
@@ -49,18 +52,15 @@ def index(request):
 	vip_jobs = Job.objects.filter(id__in=job_id_list)
 	job_list = []
 	for my_job in vip_jobs:
-		profile = Profile.objects.filter(id=my_job.user_id)[0]
-		username = profile.real_name
-		portrait = profile.portrait
 		city = my_job.city + " " + my_job.district
 		job = {'city': city, 'company_name': my_job.company_name, 'job_title': my_job.job_title,
 		       'education': my_job.education, 'work_experience': my_job.work_experience, 'salary': my_job.salary,
-		       'create_time': convert.format_time(my_job.create_time), 'username': username, 'portrait': portrait,
+		       'create_time': convert.format_time(my_job.create_time), 'username': profile.real_name, 'portrait': profile.portrait,
 		       'job_uuid': my_job.uuid}
 		job_list.append(job)
-	print own_job
+	
 	page_data = {'own_job': json.dumps(own_job), 'job_list': json.dumps(job_list)}
-	# 请把own_jobs和vip_jobs渲染到页面上
+	# 请把own_job和vip_jobs渲染到页面上
 	if vip_job_from_point == 0:  # 首页，需要返回页面
 		return render_to_response('index.html', page_data)
 	else:  # 加载下一页，ajax请求
@@ -104,8 +104,6 @@ def get_job(request):
 
 # 暂时规避跨站攻击保护
 from django.views.decorators.csrf import csrf_exempt
-
-
 @csrf_exempt
 @sns_userinfo_with_userinfo
 def post_job(request):
